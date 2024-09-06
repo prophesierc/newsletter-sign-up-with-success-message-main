2759928ec34ce8e675c52f75776c89a577a9237a
ProphesierC <bwknowles98@gmail.com>
2024-09-05 21:07:30 -0400

    update 9 files Initialized Vuetify for updated textfields

--- app/src/components/EmailField.vue
+++ app/src/components/EmailField.vue
@@ -2,31 +2,47 @@
   <h3 class="email__title">
     Email address
   </h3>
-  <input type="text" placeholder="email@company.com" class="email__input"/>
+
+  <v-text-field
+    :rules="[rules.required]"
+    label="email@company.com"
+    class="email__input"
+    bg-color="white"
+    clearable
+    variant="outlined"
+  ></v-text-field>
+
 </template>
 
 <script>
-export default {
-  name: 'EmailField'
+export default 
+{
+  name: 'EmailField',
+  data: () => 
+  (
+    {
+      rules: 
+      {
+        required: value => !!value || 'Field is required',
+      },
+    }
+  )
 }
 </script>
 
-<style>
-  .email__title{
+<style scoped>
+  .email__title {
     font-size: 12px;
     color: var(--neutral-clr-Charcoal-Grey);
     margin: 3em 0 .5em 0;
   }
 
-  .email__input{
+  .email__input :deep(.v-input__control) {
     width: 100%;
-    height: 3.2em;
-    border: 1px solid var(--neutral-clr-Grey);
+    background-color: var(--neutral-clr-White);
+  }
+
+  .email__input :deep(.v-field__input) {
     color: var(--neutral-clr-Grey);
-    border-radius: 8px;
-    padding-left: 1.5em;
-    font-size: 16px;  
-    font-family: inherit;
-    margin: 0 0 .5em 0;
   }
 </style>
