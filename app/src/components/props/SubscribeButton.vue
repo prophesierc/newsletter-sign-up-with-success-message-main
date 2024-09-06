fbdf71eec1a5aabeeca7b12c324719b719e9ffea
ProphesierC <bwknowles98@gmail.com>
2024-09-05 21:34:45 -0400

    update SubscribeButton.vue Initalized open-modal handler and overwrit Vuetify btn styling

--- app/src/components/props/SubscribeButton.vue
+++ app/src/components/props/SubscribeButton.vue
@@ -4,27 +4,39 @@
   type="submit"
   density="default"
   size="large"
+  @click="modalHandler"
   >
   {{ buttonText }}</v-btn> 
 </template>
 
 <script>
-  export default {
-    props: ['buttonText']
+export default 
+{
+  props: ['buttonText'],
+  methods: 
+  {
+    handleClick() 
+    {
+      this.$emit('open-modal');
+    }
   }
-// needs logic to open modal on click event
+}
 </script>
 
-<style>
+<style scoped>
   .subscribe__button{
     width: 100%;
     background-color: var(--neutral-clr-Dark-Slate-Grey);
     color: var(--neutral-clr-White);
     border-radius: 8px;
     border: none;
-    font-size: 11px;
+    font-size: 12px;
     font-weight: var(--fw-700);
     font-family: inherit;
     margin: .5em 0 0 0;
+  }
+  .subscribe__button :deep(.v-btn__content) {
+    color: var(--neutral-clr-White) !important;
+    text-transform: none !important;
   }
 </style>
