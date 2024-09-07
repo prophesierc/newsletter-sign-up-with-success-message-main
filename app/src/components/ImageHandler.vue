d6fea4aaa3955fe65b568cd296d24372bf8b3264
ProphesierC <bwknowles98@gmail.com>
2024-09-07 06:48:43 -0400

    update App.vue, ImageHandler.vue and global.css

--- app/src/components/ImageHandler.vue
+++ app/src/components/ImageHandler.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header__container">
     <picture class="header__picture">
-      <source media="(max-width: 699px)" srcset="@/assets/images/illustration-sign-up-mobile.svg">
-      <source media="(min-width: 700px)" srcset="@/assets/images/illustration-sign-up-desktop.svg">
+      <source media="(max-width: 799px)" srcset="@/assets/images/illustration-sign-up-mobile.svg">
+      <source media="(min-width: 800px)" srcset="@/assets/images/illustration-sign-up-desktop.svg">
       <img src="@/assets/images/illustration-sign-up-mobile.svg" alt="Header Picture">
     </picture>
   </header>
@@ -10,22 +10,47 @@
 
 <script>
 export default {
-  name: 'ImageSelector'
+  name: 'ImageHandler'
 }
 </script>
 
-<style>
+<style scoped>
 .header__container {
-  width: 100vw;
+  width: 100%;
   height: auto;
   overflow: hidden;
 }
 
-.header__picture img{
-  width: 100vw;
+.header__picture img {
+  width: 100%;
   height: auto;
-  max-height: 32vh;
   object-fit: cover;
   border-radius: 0 0 14px 14px;
+}
+
+@media (min-width: 800px) {
+  body {
+    display: flex;
+    flex-direction: row; 
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  main {
+    width: 50%;
+  }
+
+  .header__container {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .header__picture img {
+    width: auto;
+    max-width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
 }
 </style>
