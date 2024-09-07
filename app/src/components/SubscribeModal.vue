60e8c2a953a97d14ba41a22080a747abf8e3399d
ProphesierC <bwknowles98@gmail.com>
2024-09-07 06:10:34 -0400

    update SubscribeModal.vue finished mobile modal design

--- app/src/components/SubscribeModal.vue
+++ app/src/components/SubscribeModal.vue
@@ -2,8 +2,9 @@
     <transition name="slide-from-top">
         <div v-if="show" class="subscribe__modal">
           <div class="modal__content">
-            <h4>Thanks for subscribing!</h4>
-            <p>
+            <img class="modal__image" src="@/assets/images/icon-success.svg">
+            <h4 class="modal__title">Thanks for subscribing!</h4>
+            <p class="modal__text">
               A confirmation email has been sent to <b>ash@loremcompany.com</b>. 
               Please open it and click the button inside to confirm your subscription.
             </p>
@@ -40,41 +41,64 @@ export default
 </script>
 
 <style scoped>
-.slide-from-top-enter-active, .slide-from-top-leave-active {
-    transition: transform 0.5s ease-in-out, opacity 0.5s ease-in-out;
-}
+    .slide-from-top-enter-active, .slide-from-top-leave-active {
+        transition: transform 0.5s ease-in-out, opacity 0.5s ease-in-out;
+    }
 
-.slide-from-top-enter-from {
-    transform: translateY(-100%);
-    opacity: 0;
-}
+    .slide-from-top-enter-from {
+        transform: translateY(-100%);
+        opacity: 0;
+    }
 
-.slide-from-top-enter-to {
-    transform: translateY(0);
-    opacity: 1;
-}
+    .slide-from-top-enter-to {
+        transform: translateY(0);
+        opacity: 1;
+    }
 
-.slide-from-top-leave-from {
-    transform: translateY(0);
-    opacity: 1;
-}
+    .slide-from-top-leave-from {
+        transform: translateY(0);
+        opacity: 1;
+    }
 
-.slide-from-top-leave-to {
-    transform: translateY(-100%);
-    opacity: 0;
-}
+    .slide-from-top-leave-to {
+        transform: translateY(-100%);
+        opacity: 0;
+    }
 
-.subscribe__modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: var(--neutral-clr-White);
-    width: 100%;
-    height: 100%;
-}
+    .subscribe__modal {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        background-color: var(--neutral-clr-White);
+        width: 100%;
+        height: 100%;
+    }
+
+    .modal__content{
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        text-align: left;
+        font-family: var(--ff);
+        margin: 0 1em 0 1em;
+    }
+
+    .modal__image{
+        height: 50px;
+        width: 50px;
+        margin: 40% 0 3em 0;
+    }
+
+    .modal__title{
+        font-size: 34px;
+        line-height: 1em;
+        font-weight: var(--fw-700);
+        margin: 0 0 1em 0;
+    }
+
+    .modal__text{
+        margin-bottom: 96%;
+        font-size: var(--fs-paragraph);
+    }
 </style>  
