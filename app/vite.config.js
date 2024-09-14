b9896b90e5776bd2218ffb360653a5b9788da9af
ProphesierC <bwknowles98@gmail.com>
2024-09-14 04:08:14 -0400

    update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+
 // https://vitejs.dev/config/
-export default defineConfig(
-{
+export default defineConfig({
   plugins: [vue()],
-  resolve: 
-  {
-    alias: 
-    {
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, './src')
-    },
-    base: '/newsletter-sign-up-with-success-message-main/'
-  }
+    }
+  },
+  base: '/newsletter-sign-up-with-success-message-main/' 
 })
